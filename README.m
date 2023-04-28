dacf419880816be8e1ab2da5562cee6b3ac3b324
Burak <burakgoturlerr@gmail.com>
2023-04-29 00:23:28 +0300

    Update and rename README.md to README.m

--- README.m
+++ README.m
@@ -1,5 +1,5 @@
 #  About Me:
-Hello, I'm Burak. I am interested in frontend development with backend support. Recently I've been working with JavaScript and Asp.NET Core MVC.<br>I’m currently open for an a new job opportunity...
+Hello, I'm Burak. I am interested in frontend development with backend support. Recently I've been working with JavaScript and Asp.NET Core MVC.<br>I’m currently open for an a new job opportunity.
 
 
 ## 🌐 Socials:
